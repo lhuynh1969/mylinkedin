199a37f2b1c19c69d62cecf8b474b750c79cb69b
Lev Ivanov <lev.ivanov@ingka.ikea.com>
2020-08-24 16:55:16 -0400

    add rendered list of coworkers.

---
+++ src/hello.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return 'Hello, {}!'.format(name)
+
+
+@app.route("/ikea/coworkers/")
+def list_coworkers():
+    lc = [
+        ("Lev", "Ivanov"),
+        ("Linh", "Huynh"),
+        ("Jon", "Kurinsky"),
+        ("John", "Reile")
+    ]
+    return render_template("coworkers.html", coworkers_list=lc)
+
