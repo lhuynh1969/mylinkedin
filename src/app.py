e6bac3636c85ca04330592be05545ee9cd0d1be6
Lev Ivanov <lev.ivanov@ingka.ikea.com>
2020-09-29 17:23:30 -0400

    Replace Alembic by Flask-Alembic. Add initial migrations.

--- src/app.py
+++ src/app.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-from etc import settings
-
+from . import settings
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
-    f"mysql:{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+    f"mysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 )
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 
 @app.route('/')
 def hello_world():
--- src/models/user.py
+++ src/models/user.py
@@ -3,5 +3,6 @@ from src.app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
+    username = db.Column(db.String(128), unique=True, nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    first_name = db.Column(db.String(128), nullable=False)
